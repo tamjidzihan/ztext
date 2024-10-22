80c5b8ab92d07f36a949c433df0459dc2866a5c2
tamjidzihan <tamjidzihan@gmail.com>
2024-10-23 00:34:40 +0600

    home 2024-10-23

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,28 @@ import { NAVIGATION } from './router/NAVIGATION';
 import { useRouter } from './router/router';
 import { CustomTheme } from './theme/Theme';
 import OrdersPage from './pages/OrdersPage';
+import ztext_logo from './assets/ztext_logo.png'
+import { Box } from '@mui/material';
 
 
 const App = () => {
   const router = useRouter('');
+
+  const branding = {
+    title: 'Ztext', // Your brand title here
+    logo: (
+      <Box display="flex" alignItems="center">
+        <img src={ztext_logo} alt="Ztext Logo" style={{ width: '40px' }} />
+      </Box>
+    ),
+  };
 
   return (
     <AppProvider
       navigation={NAVIGATION}
       router={router}
       theme={CustomTheme}
+      branding={branding}
     >
       <DashboardLayout>
         <PageContainer>
